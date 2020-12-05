bc0e6dec0cd75d5e3e171ea46e81b9a3ceb52751
petrus <holm.petrus@gmail.com>
2020-12-05 19:08:05 +0200

    Initial Commit

---
+++ OpenInNotion/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  OpenInNotion
+//
+//  Created by Petrus Holm on 5.12.2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
